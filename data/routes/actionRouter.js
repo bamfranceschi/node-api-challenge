4e08ae80de0d00135d1a55dc212c7569f81a8957
bamfranceschi <anna.franceschi@gmail.com>
2020-01-17 10:22:32 -0600

    router methods identified and outlined

---
+++ data/routes/actionRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const Actions = require("../helpers/actionModel");
+
+const router = express.Router();
+
+//GET all actions
+
+//GET one action by id
+
+//POST new action on project
+
+//UPDATE action by id
+
+//DELETE action by id
---
+++ data/routes/projectRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const Projects = require("../helpers/projectModel");
+
+const router = express.Router();
+
+//GET all projects
+
+//GET one project by id
+
+//GET one project's actions
+
+//POST new project
+
+//UPDATE project by id
+
+//DELETE project by id
